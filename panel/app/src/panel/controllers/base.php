0f8d8cd968ec1d94f5c2aa2b59e98498ccaaabab
Jorge Fco <jorge.fco.mend@gmail.com>
2020-11-27 10:12:08 -0600

    Edit sidebar

--- panel/app/src/panel/controllers/base.php
+++ panel/app/src/panel/controllers/base.php
@@ -41,7 +41,7 @@ class Base extends Obj {
   public function layout($type, $data = array()) {
 
 	$version  = panel()->version();
-	$versioncss  = '0.06';
+	$versioncss  = '0.08';
 	$base     = panel()->urls()->index();
 	$cssbase  = panel()->urls()->css();
 	$jsbase   = panel()->urls()->js();
